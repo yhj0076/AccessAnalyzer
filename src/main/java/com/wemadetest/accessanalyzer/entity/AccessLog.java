cfe949edcce4d90d684ef0a6cb6beeb9157e9b3e
King0wl <70518135+yhj0076@users.noreply.github.com>
2026-02-01 22:39:30 +0900

    feat: 엔티티 생성

---
+++ src/main/java/com/wemadetest/accessanalyzer/entity/AccessLog.java
@@ -0,0 +1,19 @@
+﻿package com.wemadetest.accessanalyzer.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class AccessLog {
+    private int successCount;
+    private int redirectCount;
+    private int clientErrorCount;
+    private int serverErrorCount;
+
+    private List<Long> detailLogs;
+}
