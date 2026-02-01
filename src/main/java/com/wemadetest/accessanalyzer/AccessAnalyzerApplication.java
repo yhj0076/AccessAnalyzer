6a8393988b49e85ae59e9513ac3e95d5c9064e4b
King0wl <70518135+yhj0076@users.noreply.github.com>
2026-02-01 22:06:50 +0900

    프로젝트 생성

---
+++ src/main/java/com/wemadetest/accessanalyzer/AccessAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.wemadetest.accessanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wemadetest/accessanalyzer/AccessAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wemadetest.accessanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
