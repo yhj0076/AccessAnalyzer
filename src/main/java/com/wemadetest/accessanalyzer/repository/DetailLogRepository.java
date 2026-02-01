12d851b641c6b4fd77a96a735dfc5c5c2825be96
King0wl <70518135+yhj0076@users.noreply.github.com>
2026-02-01 22:43:34 +0900

    feat: 레포지토리 생성

---
+++ src/main/java/com/wemadetest/accessanalyzer/repository/DetailLogRepository.java
@@ -0,0 +1,24 @@
+﻿package com.wemadetest.accessanalyzer.repository;
+
+import com.wemadetest.accessanalyzer.entity.DetailLog;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class DetailLogRepository {
+    private long id = 0;
+    private final Map<Long, DetailLog> detailLogDatabase = new ConcurrentHashMap<Long, DetailLog>();
+    private final AccessLogRepository accessLogRepository;
+
+    public DetailLogRepository(AccessLogRepository accessLogRepository) {
+        this.accessLogRepository = accessLogRepository;
+    }
+
+    public long save(DetailLog detailLog, long analysisId) {
+        detailLogDatabase.put(id++, detailLog);
+        accessLogRepository.get(analysisId).getDetailLogs().add(id);
+        return id;
+    }
+}
