b39ef193f9172c85451fadddfb4b191bcce051e2
King0wl <70518135+yhj0076@users.noreply.github.com>
2026-02-04 16:33:47 +0900

    feat: Caffeine 캐시 설정 추가

---
+++ src/main/java/com/wemadetest/accessanalyzer/config/CacheConfig.java
@@ -0,0 +1,27 @@
+﻿package com.wemadetest.accessanalyzer.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class CacheConfig {
+    @Bean
+    public Caffeine<Object, Object> caffeine() {
+        return Caffeine.newBuilder()
+                .maximumSize(1000)
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .recordStats();
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager manager = new CaffeineCacheManager();
+        manager.setCaffeine(caffeine);
+        return manager;
+    }
+}
